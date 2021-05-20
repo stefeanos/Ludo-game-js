eb8139016804f7dddfff5c8735cfb3a9da8c80f5
stefeanos <84009599+stefeanos@users.noreply.github.com>
2021-05-20 11:59:22 +0200

    ludo

---
+++ Ludo-game1.js
@@ -0,0 +1,178 @@
+﻿colors = [
+  '#06578d',
+  'white',
+  'white',
+  'white',
+  'white',
+  '#e95605',
+  'white',
+  'white',
+  'white',
+  'white',
+  '#f9ed13',
+  'white',
+  'white',
+  'white',
+  'white',
+  '#1f8a2e',
+  'white',
+  'white',
+  'white',
+  'white',
+];
+document.querySelectorAll('circle').forEach((circle, items) => {
+  circle.style.fill = colors[items];
+});
+colors = [
+  'black',
+  'black',
+  'black',
+  'black',
+  'black',
+  'black',
+  'black',
+  'black',
+  'black',
+  'black',
+  'black',
+  'black',
+  'black',
+  'black',
+  'black',
+  'black',
+  'black',
+  'black',
+  'black',
+  'black',
+];
+document.querySelectorAll('circle').forEach((circle, items) => {
+  circle.style.stroke = colors[items];
+  //console.log(circle);
+});
+
+colors = ['#06578d', '#06578d', '#06578d', '#06578d', '#06578d', '#06578d'];
+document.querySelectorAll('rect.blue').forEach((rect, items) => {
+  rect.style.fill = colors[items];
+
+  //console.log(rect);
+});
+colors = ['black', 'black', 'black', 'black', 'black', 'black'];
+document.querySelectorAll('rect.blue').forEach((rect, items) => {
+  rect.style.stroke = colors[items];
+
+  //console.log(rect);
+});
+colors = ['#e95605', '#e95605', '#e95605', '#e95605', '#e95605', '#e95605'];
+document.querySelectorAll('rect.red').forEach((rect, items) => {
+  rect.style.fill = colors[items];
+
+  //console.log(rect);
+});
+colors = ['black', 'black', 'black', 'black', 'black', 'black'];
+document.querySelectorAll('rect.red').forEach((rect, items) => {
+  rect.style.stroke = colors[items];
+
+  //console.log(rect);
+});
+
+colors = ['#f9ed13', '#f9ed13', '#f9ed13', '#f9ed13', '#f9ed13', '#f9ed13'];
+document.querySelectorAll('rect.yellow').forEach((rect, items) => {
+  rect.style.fill = colors[items];
+
+  //console.log(rect);
+});
+
+colors = ['black', 'black', 'black', 'black', 'black', 'black'];
+document.querySelectorAll('rect.yellow').forEach((rect, items) => {
+  rect.style.stroke = colors[items];
+
+  //console.log(rect);
+});
+colors = ['green', 'green', 'green', 'green', 'green', 'green'];
+document.querySelectorAll('rect.green').forEach((rect, items) => {
+  rect.style.fill = colors[items];
+
+  //console.log(rect);
+});
+
+colors = ['black', 'black', 'black', 'black', 'black', 'black'];
+document.querySelectorAll('rect.green').forEach((rect, items) => {
+  rect.style.stroke = colors[items];
+
+  //console.log(rect);
+});
+colors = ['#1f8a2e', '#1f8a2e', '#1f8a2e', '#1f8a2e', '#1f8a2e', '#1f8a2e'];
+document.querySelectorAll('polygon.green').forEach((polygon, items) => {
+  polygon.style.fill = colors[items];
+
+  //console.log(rect);
+});
+
+colors = ['black', 'black', 'black', 'black', 'black', 'black'];
+document.querySelectorAll('polygon.green').forEach((polygon, items) => {
+  polygon.style.stroke = colors[items];
+
+  //console.log(rect);
+});
+colors = ['#06578d', '#06578d', '#06578d', '#06578d', '#06578d', '#06578d'];
+document.querySelectorAll('polygon.blue').forEach((polygon, items) => {
+  polygon.style.fill = colors[items];
+
+  //console.log(rect);
+});
+
+colors = ['black', 'black', 'black', 'black', 'black', 'black'];
+document.querySelectorAll('polygon.blue').forEach((polygon, items) => {
+  polygon.style.stroke = colors[items];
+
+  //console.log(rect);
+});
+colors = ['#e95605', '#e95605', '#e95605', '#e95605', '#e95605', '#e95605'];
+document.querySelectorAll('polygon.red').forEach((polygon, items) => {
+  polygon.style.fill = colors[items];
+
+  //console.log(rect);
+});
+
+colors = ['black', 'black', 'black', 'black', 'black', 'black'];
+document.querySelectorAll('polygon.red').forEach((polygon, items) => {
+  polygon.style.stroke = colors[items];
+
+  //console.log(rect);
+});
+colors = ['#f9ed13', '#f9ed13', '#f9ed13', '#f9ed13', '#f9ed13', '#f9ed13'];
+document.querySelectorAll('polygon.yellow').forEach((polygon, items) => {
+  polygon.style.fill = colors[items];
+
+  //console.log(rect);
+});
+
+colors = ['black', 'black', 'black', 'black', 'black', 'black'];
+document.querySelectorAll('polygon.yellow').forEach((polygon, items) => {
+  polygon.style.stroke = colors[items];
+
+  //console.log(rect);
+});
+
+//let blue = document.querySelectorAll('rect.blue ');//
+
+//const dataAttributesElement = document.querySelector('[data-test]');
+//const divsWihClasses = document.querySelectorAll('circle ');
+//const dataAttributesElement1 = document.querySelector('rect.red');
+//const divsWihClass = document.getElementsByClassName('red circle');
+//const element = document.getElementsByClassName('blue square');
+//console.log(element);
+
+//divsWihClasses[5].style.fill = 'red';//
+
+//const items = Array.from(document.querySelectorAll('circle '));
+//console.log(items);
+
+//console.log(items);
+
+//const circle = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//divsWihClasses.forEach(div => (div.style.fill = ''));
+
+//dataAttributesElement1.style.fill = 'green';
+//dataAttributesElement.style.fill = 'red';
